1320ee827845eb0013a6d8cd9d5fcdec29e351ba
pro804 <petsoulis.appdev@gmail.com>
2025-10-13 10:56:55 +0300

    feat(phase1/ui): integrate shadcn/ui library and add core components - Set up shadcn/ui library for consistent, reusable UI patterns - Added initial components: breadcrumb, card, carousel, checkbox, dropdown-menu, input, label, popover, select, separator, skeleton, table, textarea, toast, and button - Verified setup on Home page by rendering a test button with custom styles - Establish foundation for building feature-rich, styled pages in Phase 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
